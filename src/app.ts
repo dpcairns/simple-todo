dd929f47ff72bc94d8037b2cb499d652abdb1bbd
Danny Cairns <dpcairns@gmail.com>
2022-04-07 16:39:25 -0700

    initial commit

--- src/app.ts
+++ src/app.ts
@@ -1,52 +1,58 @@
-// import functions and grab DOM elements
-import { didUserWin, getRandomThrow } from './get-random-throw'
-const usersOptions = document.querySelector('#users-options')
-const playButton = document.querySelector('#play')
-const resultsSpan = document.querySelector('#results')
-const tallySpan = document.querySelector('#tally')
+type ValidTask = 'wash dishes' | 'walk the dog' | 'mow the lawn' | 'weed the garden'
 
-// initialize state
-let computersThrow = getRandomThrow()
-let wins = 0;
-let losses = 0;
-let games = 0;
-
-// set event listeners to update state and DOM 
-playButton.addEventListener('click', () => {
-  const usersPlay: HTMLInputElement = usersOptions.querySelector('input:checked')
-  
-  const gameResult = didUserWin(usersPlay.value, computersThrow)
-
-  if(gameResult === 'draw') {
-    games ++;
-
-    updateResults(wins, losses, games, `you played ${usersPlay.value} and tied our ${computersThrow}`)
-    computersThrow = getRandomThrow()
-  } 
-  if(gameResult === 'win') {
-    wins ++;
-    games ++;
-
-    updateResults(wins, losses, games, `you played ${usersPlay.value} and beat our ${computersThrow}`)
-    computersThrow = getRandomThrow()
-  } else if( gameResult === 'lose') {
-    losses ++;
-    games ++;
-
-    updateResults(wins, losses, games, `you played ${usersPlay.value} and lost to our ${computersThrow}`)
-    computersThrow = getRandomThrow()
-  }
-  
-})
-
-const updateResults = (w: number, l: number, g: number, message: string) => {
-  resultsSpan.textContent = message;
-
-  tallySpan.textContent = `
-    wins: ${wins}
-    losses: ${losses}
-    draws: ${games - (wins + losses)}
-    total games: ${games}
-  `
+type Todo = {
+  todo: ValidTask,
+  completed: boolean
 }
 
+type TodoList = Array<Todo> 
+
+const form = document.querySelector('form');
+const todoListEl = document.querySelector('.todos')
+
+const todos : TodoList = [];
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const data = new FormData(form);
+
+  const newTodoString = data.get('todo') as ValidTask;
+
+  const newTodo = {
+    todo: newTodoString,
+    completed: false
+  }
+
+  todos.push(newTodo)
+
+  displayTodos();
+
+  form.reset();
+})
+
+export function renderTodo(todo : Todo) {
+  const todoEl = document.createElement('p')
+
+  todoEl.textContent = todo.todo;
+  
+  if (todo.completed) {
+    todoEl.classList.add('completed')
+  }
+
+  return todoEl;
+}
+
+function displayTodos() {
+  todoListEl.textContent = '';
+
+  for (let todo of todos) {
+    const todoEl = renderTodo(todo);
+
+    todoListEl.append(todoEl);   
+  }
+}
+
+window.addEventListener('load', () => {
+  displayTodos();
+})
